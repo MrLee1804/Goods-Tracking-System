3567123024a25e61ef3a4f0c7386822da4bdf54c
MrLee1804 <lingeshwaran3535@gmail.com>
2025-04-02 19:47:38 +0530

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,17 @@
-
+// Basic goods tracking functionality
+function deleteGood(index) {
+    if (confirm('Are you sure you want to delete this item?')) {
+        fetch('/delete/' + index, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                window.location.reload();
+            }
+        }).catch(error => {
+            console.error('Error:', error);
+        });
+    }
+}
